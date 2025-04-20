2113ad4b6816663fbc827a0c299f02113cdae885
Akira HIGUCHI <toiroakr@gmail.com>
2025-04-21 00:24:34 +0900

    Feat/jsonc (#5)
    * use jsonc
    
    * use jsonc for backup
    
    * Squashed commit of the following:
    
    commit 2dce20d2ae985f13194b308619d67216a1169a61
    Author: Akira HIGUCHI <toiroakr@gmail.com>
    Date:   Sun Apr 20 23:59:51 2025 +0900
    
        Chore/clarify test (#4)
    
        * create new test base
    
        * add init test
    
        * add test for sync
    
        * fix project setting
    
        * add ci
    
        * update
    
        * fix
    
        * fix
    
    * update
    
    * install jsonc-parser as dependencies

--- test/init/index.test.ts
+++ test/init/index.test.ts
@@ -12,10 +12,10 @@ describe("init", () => {
 	it("case1: init from empty", test(1));
 	it("case2: settings.json already exists", test(2));
 	it(
-		"case3: settings.json and settings-project.json exist with different content",
+		"case3: settings.json and settings-project.jsonc exist with different content",
 		test(3),
 	);
 	it("case4: .gitignore exists with partial entries", test(4));
-	it("case5: settings-local.json already exists", test(5));
+	it("case5: settings-local.jsonc already exists", test(5));
 	it("case6: settings.json has complex nested settings", test(6));
 });
--- test/sync/index.test.ts
+++ test/sync/index.test.ts
@@ -15,7 +15,7 @@ describe("sync", () => {
 	it("case4: merge with array settings", test(4));
 	it("case5: merge with complex nested settings", test(5));
 	// new clone scenario
-	it("case6: only settings-project.json exists", test(6));
+	it("case6: only settings-project.jsonc exists", test(6));
 	// integration scenario for existing projects
-	it("case7: settings.json differs from settings-project.json", test(7));
+	it("case7: settings.json differs from settings-project.jsonc", test(7));
 });
